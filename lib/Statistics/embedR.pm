14fa80377bfaddc3eb7c46186a8e73ffaadf603b
Hongwen Qiu <qiuhongwen@gmail.com>
2010-11-11 17:13:41 +0800

    Initial import.

---
+++ lib/Statistics/embedR.pm
@@ -0,0 +1,110 @@
+package Statistics::embedR;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Statistics::embedR - The great new Statistics::embedR!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Statistics::embedR;
+
+    my $foo = Statistics::embedR->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Hongwen Qiu, C<< <qiuhongwen at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-statistics-embedr at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Statistics-embedR>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Statistics::embedR
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Statistics-embedR>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Statistics-embedR>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Statistics-embedR>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Statistics-embedR/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Hongwen Qiu.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Statistics::embedR
